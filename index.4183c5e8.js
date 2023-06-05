b1303dfb490d90157c99ded379c8758b438d410d
MichalSzmajduch <126428472+MichalSzmajduch@users.noreply.github.com>
2023-06-05 21:41:41 +0000

    Deploying to gh-pages from @ MichalSzmajduch/projekt-4@e1905eef7239492fde1821b9f1fc4d2cefc568ca 🚀

--- index.4183c5e8.js
+++
@@ -1,2 +0,0 @@
-const openModalButton=document.querySelector(".modal__open"),closeModalButton=document.querySelector(".modal__close"),modal=document.querySelector(".modal");openModalButton.addEventListener("click",(()=>{modal.classList.remove("is-hidden")})),closeModalButton.addEventListener("click",(()=>{modal.classList.add("is-hidden")})),z;
-//# sourceMappingURL=index.4183c5e8.js.map
